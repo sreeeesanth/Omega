954016683a82489ea2565f4570b2cb59cb6b8fcf
Aaron <138325645+Aaron-53@users.noreply.github.com>
2025-09-24 18:42:26 +0530

    fixed animation repetition

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client";  // 👈 Add this at the very top
+"use client"; // 👈 Add this at the very top
 
 import {
   About,
@@ -21,7 +21,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    AOS.init({ duration: 800, once: true });
+    AOS.init({ duration: 800 });
   }, []);
 
   return (
