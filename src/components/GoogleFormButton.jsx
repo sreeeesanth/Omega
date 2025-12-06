b697f8770d8aed300a5b6161361e6c2b402f6846
Sree <82359295+sreeeesanth@users.noreply.github.com>
2025-12-06 10:59:57 +0530

    Change button text to 'REGISTRATIONS CLOSED'

--- src/components/GoogleFormButton.jsx
+++ src/components/GoogleFormButton.jsx
@@ -8,11 +8,10 @@ const GoogleFormButton = ({ className = "" }) => {
   return (
     <div className="flex justify-center mt-8">
       <button
-        onClick={handleClick}
         data-aos="fade-up"
         className={`z-40 px-10 py-5 m-2 bg-[#d53232] text-white font-bold rounded-lg hover:bg-[#e32e2e] transition duration-300 ${className}`}
       >
-        REGISTER NOW
+        REGISTRATIONS CLOSED
       </button>
     </div>
   );
